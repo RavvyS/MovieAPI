06bf6c86b5cb8292d0b208c16258efea9609773e
RavvyS <156438913+RavvyS@users.noreply.github.com>
2025-01-12 01:40:35 +0530

    NavBar complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import Home from './pages/Home.jsx';
+import{Routes ,Route} from 'react-router-dom';
 import './App.css'
+import Favorites from './pages/Favorites.jsx';
+import NavBar from './components/NavBar.jsx';
 
 function App() {
  
 
 
-  return (<>
- <Home/>
-  </>
+  return (
+   <div>
+    <NavBar/>
+<main className='main-content'>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path="/Favorites" element={<Favorites/>}> </Route>
+
+  </Routes>
+</main>
+</div> 
   ); 
 }
 export default App;
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites (){
+    return (
+    <div className="favourites-empty">
+        <h2>No Favorites  Movies Yet</h2>
+        <p>Start adding movies to your Favorites any they will appear here!</p>
+    </div>
+    )
+}
+
+export default Favorites;
