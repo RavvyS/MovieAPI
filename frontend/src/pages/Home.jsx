6e735363c4d3ad1dfd4c11ca830f1c4b03e416ec
RavvyS <156438913+RavvyS@users.noreply.github.com>
2025-01-12 13:13:23 +0530

    api fetch done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,47 +1,63 @@
-        import MovieCard from "../components/MovieCard.jsx";
-        import { useState } from "react";
-        import "../CSS/Home.css";
+import MovieCard from "../components/MovieCard.jsx";
+import { useState,useEffect } from "react";
+import {searchMovies, getPopularMovies} from "../services/api.js";
+import "../CSS/Home.css";
 
-        function Home(){
-            const [searchQuery,setSearchQuery] =useState("");
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const[movies,setMovies]=useState([]);
+    const [error,setError] =useState(null);
+    const [loading,setLoading]=useState(true);
 
+    useEffect(() => {
+        const loadPopularMovies =async () =>{
+            try{
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
 
-       
-        const movies =[
-            {id: 1, title: "John Wick", release_date: "2020"},
-            {id: 2, title: "HarryPotter", release_date: "2320"},
-            {id: 3, title: "John ", release_date: "2020"},
-
-        ]
-        const handleSearch =(e) => {
-            e.preventDefault()
-            alert(searchQuery)
-            setSearchQuery("-------")
-
+            }catch (err){
+                console.log(err);
+                setError("Failed to load Movies");
+                
+            }finally{
+                setLoading(false);
+            }
         }
+        loadPopularMovies();
+    }, []);
 
-            return <div className="home">
-                <form onSubmit={handleSearch} className="search-form">
-        <input 
-        type="text" 
-        placeholder="Search for movies..." 
-        className="search-input"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}/>
 
-        <button type="submit" className="search-button">Search</button>
 
-                </form>
 
-                <div className="movies-grid">
-                    {movies.map(movie => 
-                   // movie.title.toLowerCase().startsWith(searchQuery) && 
-                    
-                    (<MovieCard movie={movie} key={movie.id}/>)
-                )}
-                </div>
-            </div>
-        }
-        
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("-------")
 
-        export default Home;
+    }
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input
+                type="text"
+                placeholder="Search for movies..."
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
+
+            <button type="submit" className="search-button">Search</button>
+
+        </form>
+
+        <div className="movies-grid">
+            {movies.map(movie =>
+                // movie.title.toLowerCase().startsWith(searchQuery) && 
+
+                (<MovieCard movie={movie} key={movie.id} />)
+            )}
+        </div>
+    </div>
+}
+
+
+export default Home;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,17 @@
 const API_KEY = "6c2d12b6ed24ad1609be8db0d984ad96";
-const BASE_URL = "https://www.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch("${BASE_URL}/movie/popular?api_key=${API_KEY}");
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    "${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}"
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
   );
   const data = await response.json();
   return data.results;
